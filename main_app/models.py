0219459028b9028e2f5a6bd5bc9e29e9dfb9eb40
farnaz <farnaz.towhidi@gmail.com>
2022-11-10 12:38:27 -0500

    Creating model

---
+++ main_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=50)
+    reps = models.IntegerField()
+    set = models.IntegerField()
+    notes = models.CharField(max_length=50)
+    picture = models.CharField(max_length=50)
+
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+    weight = models.IntegerField()
+    height = models.IntegerField()
+
+
+class ClientExercise(models.Model):
+    name = models.DateField('2022-22-01')
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+
