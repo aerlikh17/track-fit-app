3259fe5d6c76ce5aed1523fc35c155ec63602e5f
farnaz <farnaz.towhidi@gmail.com>
2022-11-11 21:16:22 -0500

    Add user model

--- main_app/models.py
+++ main_app/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 class Exercise(models.Model):
     name = models.CharField(max_length=50)
-    reps = models.IntegerField()
-    set = models.IntegerField()
-    notes = models.CharField(max_length=50)
+    unitType = models.CharField(max_length=50)
+    Description = models.CharField(max_length=100)
     picture = models.CharField(max_length=50)
 
 
@@ -14,8 +13,12 @@ class Client(models.Model):
     height = models.IntegerField()
 
 
+
 class ClientExercise(models.Model):
     Date = models.DateField('2022-22-01')
+    Note = models.CharField(max_length=100)
+    unitAmount1 = models.IntegerField()
+    unitAmount2 = models.IntegerField()
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
 
