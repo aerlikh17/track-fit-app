fc027fa5e35e115e7a1ba123494cb98a2acfad1b
farnaz <farnaz.towhidi@gmail.com>
2022-11-15 16:45:42 -0500

    update style

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('clientExercise/<int:user_id>', views.loglist, name='log'),
   path('clientExercise/add/<int:user_id>/<int:exercise_id>', views.logAdd, name='logAdd'),
+  clientExercise/Edit/
   path('accounts/', include('django.contrib.auth.urls')),
   path('accounts/signup', views.signup, name='signup'),
   path('exercise/<int:pk>/delete', views.ExerciseDelete, name='exercise-delete'),
--- main_app/views.py
+++ main_app/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from .forms import ClientExerciseForm
 from datetime import date
+
 import uuid
 import boto3
 
@@ -19,8 +20,8 @@ def home(request):
 def loglist(request, user_id):
     today = date.today()
     today_date = today.strftime("%B %d, %Y")
-    clientExercise = ClientExercise.objects.filter (user_id = user_id ).select_related('exercise')
-    exercise = Exercise.objects.exclude (id__in = ClientExercise.objects.filter (user_id = user_id ).values_list('exercise_id')) 
+    clientExercise = ClientExercise.objects.filter (user_id = user_id, date = today ).select_related('exercise')
+    exercise = Exercise.objects.exclude (id__in = ClientExercise.objects.filter (user_id = user_id, date = today ).values_list('exercise_id')) 
     return render(request, 'clientExercise/log.html', {'clientExercise': clientExercise, 'today_date': today_date, 'exercise': exercise} )
 
 
