22a92fc35dca26613f609b687971afc8de122642
farnaz <farnaz.towhidi@gmail.com>
2022-11-15 00:24:00 -0500

    update on adding daily routine

--- main_app/views.py
+++ main_app/views.py
@@ -11,9 +11,23 @@ import uuid
 import boto3
 
 
-
 def home(request):
     return render(request, 'home.html')
+
+
+def loglist(request, user_id):
+    today = date.today()
+    today_date = today.strftime("%B %d, %Y")
+    clientExercise = ClientExercise.objects.filter (user_id = user_id ).select_related('exercise')
+    exercise = Exercise.objects.exclude (id__in = ClientExercise.objects.filter (user_id = user_id ).values_list('exercise_id')) 
+    return render(request, 'clientExercise/log.html', {'clientExercise': clientExercise, 'today_date': today_date, 'exercise': exercise} )
+
+
+def logAdd (request, user_id, exercise_id):
+    form = AddExecForm(request.POST)
+    new_ClientExercise = form.save(commit=False)
+    return redirect('log', user_id = user_id)
+
 
 def signup(request):
   error_message = ''
@@ -34,13 +48,4 @@ def signup(request):
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
-
-def loglist(request, user_id):
-    today = date.today()
-    today_date = today.strftime("%B %d, %Y")
-    clientExercise = ClientExercise.objects.filter (user_id = user_id ).select_related('exercise')
-    exercise = Exercise.objects.exclude (id__in = ClientExercise.objects.filter (user_id = user_id ).values_list('exercise_id'))
-    
-  
-    return render(request, 'clientExercise/log.html', {'clientExercise': clientExercise, 'today_date': today_date, 'exercise': exercise} )
 
