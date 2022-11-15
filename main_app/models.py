491dae23942c757936faacdf713ebb1bf66eb912
aerlikh <aerlikh2011@gmail.com>
2022-11-15 10:45:59 -0500

    delete function

--- main_app/models.py
+++ main_app/models.py
@@ -5,26 +5,17 @@ from django.contrib.auth.models import User
 
 class Exercise(models.Model):
     name = models.CharField(max_length=50)
-    unitType = models.CharField(max_length=50)
-    Description = models.CharField(max_length=100)
     picture = models.CharField(max_length=50)
-    
-    def __str__(self):
-        return self.name
-
-class Client(models.Model):
-    name = models.CharField(max_length=50)
-    weight = models.IntegerField()
-    height = models.IntegerField()
-
-    def __str__(self):
-        return self.name
+    description = models.CharField(max_length=100)
 
 class ClientExercise(models.Model):
-    Date = models.DateField('2022-22-01')
-    Note = models.CharField(max_length=100)
-    unitAmount1 = models.IntegerField()
-    unitAmount2 = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField('2022-22-01')
+    reps = models.IntegerField()
+    sets = models.IntegerField()
+    time = models.IntegerField()
+    note = models.CharField(max_length=100)
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.user.username} - {self.date}"
