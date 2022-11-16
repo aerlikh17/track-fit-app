40092b5d66ea637a32c08808bedd587fff350bb7
aerlikh <aerlikh2011@gmail.com>
2022-11-16 14:12:27 -0500

    adding login template

--- ExerciseApp/settings.py
+++ ExerciseApp/settings.py
@@ -125,4 +125,4 @@ Images_URL = 'static/images/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login'
