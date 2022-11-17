6be157f9d3cb1f7ae9180091fcf3610b58a53a1f
wadoichimoji <114190112+wadoichimoji@users.noreply.github.com>
2022-11-17 14:53:02 -0500

    update

--- ExerciseApp/settings.py
+++ ExerciseApp/settings.py
@@ -127,5 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
